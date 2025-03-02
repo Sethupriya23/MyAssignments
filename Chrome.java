e3df5033899c9543a484a05117d321efe1340dcb
Sethupriya23 <sethupriyaagn23@gmail.com>
2025-03-02 03:23:47 +0000

    Add files via upload
    DataType

---
+++ Chrome.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		    int year=2008;
+		    		
+	        float speed=91.0f;
+			
+			boolean isAWeb=false;
+			
+			String logo="C";
+	
+			String name="Google";
+			System.out.println("THE LOGO OF THE GOOGLE IS:"+logo);
+			
+			System.out.println("Year:"+year);
+			
+			System.out.println("Name of the Web:"+name);
+		
+		
+			
+	}
+}
